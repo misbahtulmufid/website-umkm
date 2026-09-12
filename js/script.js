856ebbf7d3e59a9e735989c175a71864b39822b0
misbahtulmufid <muhammad.azril.misbahtul25115@mhs.uingusdur.ac.id>
2026-09-12 21:31:54 +0700

    feat: tambah informasi jam buka dan lokasi

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-  promoButton.textContent = "Promo: Beli 2 gratis tester!";
-  console.log("Promo Kopi Nusa berhasil ditampilkan.");
+  promoButton.textContent = "Promo: Beli 2 gratis biji kopi!";
+  console.log("Promo Kopi Tani Laras berhasil ditampilkan.");
 });
